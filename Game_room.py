65289b39835ffd9ea0e2abfa72abaf8a39b9cc3e
amitakapita <96517259+amitakapita@users.noreply.github.com>
2022-01-30 20:41:23 +0200

    Version 1.0.5 - Working on the game_rooms manage class on Game_room.py

--- Game_room.py
+++ Game_room.py
@@ -1,13 +1,28 @@
 import random
 import threading
 
+colors = ["red", "blue", "green", "yellow"]
+
 
 class GameRoom (object):
     def __init__(self, leader_name, maximum_players):
         self.session_id = random.randint(10000, 100000)
         self.leader_name = leader_name
-        self.players = [{self.leader_name}]  # a list of dictionaries
+        self.players: dict = {}  # a list of dictionaries [{self.leader_name}]
         self.maximum_players = maximum_players
+        self.current = "creating"
+        self.count_players = 1
+
+    def join_a_player(self, player_name):
+        if self.count_players > self.maximum_players:
+            self.players[player_name] = {"id": self.count_players, "materials": {"grain": 0, "lumber": 0, "brick": 0, "wool": 0, "ore": 0, "development_card": 0}, "points": 0, "color": colors[self.count_players], "is_my_turn": False}
+            self.count_players += 1
+
+    def player_exits_the_room(self, player_name):
+        self.count_players -= 1
+        del self.players[player_name]
 
     def create_lobby(self):
         # self.players.append({self.leader_name})
+        self.current = "waiting"
+
